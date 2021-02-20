bd6ad94c790d88bbfc2a680af0b770611840c429
honne02 <anttopia@mail.ru>
2021-02-20 16:26:23 +0000

    9.2

---
+++ 9 Строковый тип данных/9.1 Индексация/ФИО.py
@@ -0,0 +1,5 @@
+s = ''
+for _ in range(3):
+    i = input()
+    s = s + i[0]
+print(s[1] + s[0] + s[2])
---
+++ 9 Строковый тип данных/9.1 Индексация/Цифра 2.py
@@ -0,0 +1,6 @@
+n = input()
+s = 'Цифр нет'
+for i in range(len(str(n))):
+    if n[i] in '0123456789':
+        s = 'Цифра'
+print(s)
