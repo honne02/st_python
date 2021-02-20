971a7a74b505f6da90b1cba120790f935af281c4
honne02 <anttopia@mail.ru>
2021-02-20 14:29:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+s = input()
+
+out = ''
+a,b = 0,0
+i=0
+while(i<len(s)):
+  if(s[i] == 'h'):
+    out += s[a:i+1]
+    a = i
+    b = (s[i+1:]).find('h')
+    p = s[a: b+1]
+    out += p[::-1]
+    i = b+1
+  else: out+=s[i]
+  i+=1
+print(out)
