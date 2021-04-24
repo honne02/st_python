c09a73411ee0020e58002e4271d1bcebcb3d72ef
honne02 <>
2021-04-24 15:46:01 +0000

    13.1 & 13.2

--- 13 Функции/13.2 Функции с параметрами/Звездный треугольник.py
+++ 13 Функции/13.2 Функции с параметрами/Звездный треугольник.py
@@ -1,7 +1,13 @@
+# объявление функции
 def draw_triangle(fill, base):
     l = [i for i in range(1,base//2+2,)]
     l.extend([i for i in range(base//2, 0, -1)])
     for elem in l:
         print(fill * elem)
 
-draw_triangle(input(), int(input()))
+# считываем данные
+fill = input()
+base = int(input())
+
+# вызываем функцию
+draw_triangle(fill, base)
