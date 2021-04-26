37309c691b3414ea6377f43d98a709b6bde1fd52
honne02 <42402582+honne02@users.noreply.github.com>
2021-04-26 19:39:50 +0300

    Delete 10 Итоговая работа на строки directory
    del

--- 10 Итоговая работа на строки/10.2 Часть 2/Каждый третий.py
+++
@@ -1,7 +0,0 @@
-s = input()
-for i in range(1,len(s)):
-    print(s[i] if i%3!=0 else "", end='')
-
-'''s = input()
-for i in range(1,len(s)):
-    print(s[i] if i%3!=0 else "", end='')'''
--- 10 Итоговая работа на строки/10.2 Часть 2/Переворот.py
+++
@@ -1,2 +0,0 @@
-s = input()
-print(s[0:s.find('h') + 1] + str(s[s.find('h') + 1:s.rfind('h')])[::-1] + s[s.rfind('h'):])
